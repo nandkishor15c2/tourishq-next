d77cb00677a08352d7b36683ab14ec01fd4cc968
nandkishor15c2 <nandkishor15shekhawat@gmail.com>
2026-05-26 20:18:00 +0000

    Remove airtable integration

--- src/lib/airtable.ts
+++ src/lib/airtable.ts
@@ -1,5 +1,4 @@
-import Airtable from 'airtable';
-
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID || '');
-
-export default base;
+// import Airtable from 'airtable';
+// const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID || '');
+// export default base;
+export default {};
